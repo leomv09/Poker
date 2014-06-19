d9fdd5afadcde5c27820d30f6bc95725ab289e30
Marcelo <marcelobarrantes94@gmail.com>
2014-06-19 13:32:49 -0600

    Agregacion de interface Deck

---
+++ Servidor/Servidor/src/poker/Deck.java
@@ -0,0 +1,17 @@
+/*
+ /*
+ * Instituto Tecnol�gico de Costa Rica
+ * Escuela de Ingenier�a en Computaci�n
+ * Lenguajes de Programaci�n
+ * Casa de Apuestas de Eddy
+ */
+
+package poker;
+
+/**
+ *
+ * @author Marcelo
+ */
+public interface Deck {
+    
+}
