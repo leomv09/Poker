c7e1b679bb691eb7891129b54d0f02aaea13c42a
Leonardo <lmadrigal09@gmail.com>
2014-06-19 19:21:42 -0600

    Actualización cliente

--- Servidor/Cliente/src/cliente/Cliente.java
+++ Servidor/Cliente/src/cliente/Cliente.java
@@ -14,7 +14,9 @@ public class Cliente {
     private String idMesa;//id de la mesa en la que está jugando.
     private SocketCliente socketCliente;//Socket por el cuál se va a comunicar con el server.
     
-    
+    /*
+     * Constructor
+     */
     public Cliente()
     {
         this.idJugador = "-1";
@@ -22,9 +24,74 @@ public class Cliente {
         this.socketCliente = new SocketCliente();
     }
     
-    private void solicitarMesas()
+    /*
+     * Método que establece el id del jugador.
+     * Parámetros: id El id del jugador. 
+     */
+    public void setidJugador(String id)
+    {
+        this.idJugador = id;
+    }
+    
+    /*
+     * Método que establece el id de mesa.
+     * Parámetros: id El id de la mesa. 
+     */
+    public void setidMesa(String id)
+    {
+        this.idMesa = id;
+    }
+    
+    /*
+     * Método que obtiene el id del jugador.
+     * Retorna: El id del jugador.
+     */
+    public String getidJugador()
+    {
+        return this.idJugador;
+    }
+    
+    /*
+     * Método que obtiene el id de la mesa.
+     * Retorna: El id de la mesa. 
+     */
+    public String getidMesa()
+    {
+        return this.idMesa;
+    }
+    
+/*
+ * Método que le solicita al servidor las mesas disponibles.
+ */
+    public void solicitarMesas()
     {
         String[] args = {""};
         this.socketCliente.enviarDato("obtenerMesas", args);
+    }
+    
+    /*
+     * Método que le solcita al servidor ingresar a una mesa.
+     * Parámetros: idMesa El id de la mesa a unirse.
+     */
+    public void unirseMesa(String idMesa)
+    {
+        String[] args = {idMesa, this.idJugador};//Se envía el id de la mesa a unirse y el jugador a unirse.
+        this.socketCliente.enviarDato("unirseMesa", args);
+    }
+    
+    /*
+     * Método que le solicita al servidor un cambio de carta.
+     * Parámetros: carta Objeto de tipo carta que indica la carta que va a cambiar.
+     * 
+     */
+    public void cambiarCarta()
+    {
+        
+    }
+    
+    public void realizarApuesta(int apuesta)
+    {
+        String[] args = {idJugador, Integer.toString(apuesta)};
+        this.socketCliente.enviarDato("apostar", args);
     }
 }
