9e8cee04202590ea6954178bb03898a473464029
Marcelo <marcelobarrantes94@gmail.com>
2014-06-21 12:53:57 -0600

    Agregacion de mano a jugador y creacion de constructor

---
+++ Servidor/ClasesCompartidas/src/poker/Mano.java
@@ -0,0 +1,40 @@
+/*
+ /*
+ * Instituto Tecnol�gico de Costa Rica
+ * Escuela de Ingenier�a en Computaci�n
+ * Lenguajes de Programaci�n
+ * Casa de Apuestas de Eddy
+ */
+
+package poker;
+
+import java.util.List;
+
+public abstract class Mano implements Comparable<Mano>{
+    List<Carta> cartas;
+    
+    public void agregarCarta(Carta c)
+    {
+        cartas.add(c);
+    }
+    
+    public int obtenerValor()
+    {
+        return 0;
+    }
+    
+    public int compareTo(Mano mano)
+    {
+        int resultado=0;
+        if (this.obtenerValor()>mano.obtenerValor())
+        {
+            resultado=1;
+        }
+        else if (this.obtenerValor()<mano.obtenerValor()){resultado=-1;}
+        else
+        {
+            resultado=0;
+        }        
+        return resultado;
+    }
+}
